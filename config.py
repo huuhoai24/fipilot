82c049c680c512c4e0029d676e5f5815cdd684d2
hoainh <nguyenhuuhoai0805@gmail.com>
2026-05-14 13:40:45 +0700

    inital env

---
+++ config.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+import os
+
+@dataclass(frozen=True)
+class Config:
+    hf_token: str= os.environ["HF_TOKEN"] 
+
+
+cfg = Config()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fipilot!")
+
+
+if __name__ == "__main__":
+    main()
